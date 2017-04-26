253fa56a633406c218d9203c0b7868e2264208ab
shanyang <shanyang@qq.com>
2017-04-27 07:49:57 +0800

    Progress component test

--- src/Progress.test.js
+++ src/Progress.test.js
@@ -4,9 +4,22 @@ import {shallow} from 'enzyme';
 import Progress from './Progress.js'
 
 
-test('it should has greater class when real > plan ',()=>{
+test('it should has a greater class when real > plan ',()=>{
   const progress = shallow(
     <Progress plan={0.5} real={0.6}/>
   );
   expect(progress.find('.greater').length).toEqual(1);
-})
+});
+test('it should has a equal class when real = plan ',()=>{
+  const progress = shallow(
+    <Progress plan={0.5} real={0.5}/>
+  );
+  expect(progress.find('.equal').length).toEqual(1);
+});
+test('it should has a less class when real < plan ',()=>{
+  const progress = shallow(
+    <Progress plan={0.6} real={0.5}/>
+  );
+  expect(progress.find('.less').length).toEqual(1);
+});
+
